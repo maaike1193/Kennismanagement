75b263c8492fdb28fa5c70c1524995ccedae26fc
robert048 <robert>
2016-04-28 13:29:28 +0200

    hakobsdeel

--- WorQitService/WorQitService/Controllers/EmployeeController.cs
+++ WorQitService/WorQitService/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.Data.SqlClient;
 using System.Web.Http;
 
@@ -26,7 +27,7 @@ namespace WorQitService.Controllers
                 SqlDataReader reader = command.ExecuteReader();
                 while (reader.Read())
                 {
-                    if (password == reader.GetValue(2).ToString());
+                    if (password == reader.GetValue(2).ToString()) ;
                     {
                         employee.ID = Convert.ToInt32(reader.GetValue(0));
                         employee.firstName = reader.GetValue(1).ToString();
@@ -34,6 +35,52 @@ namespace WorQitService.Controllers
                 }
             }
             return Json(new { Result = result, Gerbuiker = employee });
+        }
+
+        [HttpGet]
+        [ActionName("GetEmployeeByID")]
+        public Employee Get(int id)
+        {
+            //return listEmp.First(e => e.ID == id);  
+            SqlDataReader reader = null;
+            SqlConnection myConnection = new SqlConnection();
+            myConnection.ConnectionString = @"Server=tcp:worqit.database.windows.net;Database=WorQit;
+            User ID=WorQit@WorQit;Password=Stenden123;Trusted_Connection=False;
+            Encrypt=True;";
+
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.CommandType = CommandType.Text;
+            sqlCmd.CommandText = "Select * from Employee where ID=" + id + "";
+            sqlCmd.Connection = myConnection;
+            myConnection.Open();
+            reader = sqlCmd.ExecuteReader();
+            Employee emp = null;
+            while (reader.Read())
+            {
+                emp = new Employee();
+                emp.ID = Convert.ToInt32(reader.GetValue(0));
+                emp.firstName = reader.GetValue(1).ToString();
+            }
+            return emp;
+            myConnection.Close();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public void AddEmployee(string name)
+        {
+
+            SqlConnection myConnection = new SqlConnection();
+            myConnection.ConnectionString = @"Server=tcp:worqit.database.windows.net;Database=WorQit;
+                User ID=WorQit@WorQit;Password=Stenden123;Trusted_Connection=False;Encrypt=True;";
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.CommandType = CommandType.Text;
+            sqlCmd.CommandText = "INSERT INTO Employee (FirstName) Values (@Name)";
+            sqlCmd.Connection = myConnection;
+            sqlCmd.Parameters.AddWithValue("@Name", name);
+            myConnection.Open();
+            int rowInserted = sqlCmd.ExecuteNonQuery();
+            myConnection.Close();
         }
     }
 }
