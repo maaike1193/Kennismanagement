623fcceae58b2d56784c80e3bbeed6ef741f3261
maaike1193 <maaikeveurink@hotmail.com>
2016-04-28 14:03:56 +0200

    dashboard
    dashboard, alle pagina's aangemaakt plus doorlinking in het menu.

--- WorQit/Website/php/profiel.php
+++ WorQit/Website/php/profiel.php
@@ -2,8 +2,8 @@
 /**
  * Created by PhpStorm.
  * User: maaike
- * Date: 26-4-2016
- * Time: 09:40
+ * Date: 28-4-2016
+ * Time: 13:22
  */
 ?>
 
@@ -14,22 +14,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="Dashboard">
+    <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
 
-    <title>Dashboard WorQit</title>
+    <title>DASHGUM - Bootstrap Admin Template</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="dashgum/Theme/assets/css/bootstrap.css" rel="stylesheet">
+    <link href="../dashgum/Theme/assets/css/bootstrap.css" rel="stylesheet">
     <!--external css-->
-    <link href="dashgum/Theme/assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
-    <link rel="stylesheet" type="text/css" href="dashgum/Theme/assets/css/zabuto_calendar.css">
-    <link rel="stylesheet" type="text/css" href="dashgum/Theme/assets/js/gritter/css/jquery.gritter.css" />
-    <link rel="stylesheet" type="text/css" href="dashgum/Theme/assets/lineicons/style.css">
-
+    <link href="../dashgum/Theme/assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+        
     <!-- Custom styles for this template -->
-    <link href="dashgum/Theme/assets/css/style.css" rel="stylesheet">
-    <link href="dashgum/Theme/assets/css/style-responsive.css" rel="stylesheet">
-
-    <script src="dashgum/Theme/assets/js/chart-master/Chart.js"></script>
+    <link href="../dashgum/Theme/assets/css/style.css" rel="stylesheet">
+    <link href="../dashgum/Theme/assets/css/style-responsive.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -50,25 +46,25 @@
                   <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
               </div>
             <!--logo start-->
-            <a href="index.php" class="logo"><b>WorQit</b></a>
+            <a href="../index.php" class="logo"><b>WorQit</b></a>
             <!--logo end-->
             <div class="nav notify-row" id="top_menu">
                 <!--  notification start -->
                 <ul class="nav top-menu">
                     <!-- inbox dropdown start-->
                     <li id="header_inbox_bar" class="dropdown">
-                        <a data-toggle="dropdown" class="dropdown-toggle" href="index.php#">
+                        <a data-toggle="dropdown" class="dropdown-toggle" href="../index.php#">
                             <i class="fa fa-envelope-o"></i>
                             <span class="badge bg-theme">5</span>
                         </a>
                         <ul class="dropdown-menu extended inbox">
                             <div class="notify-arrow notify-arrow-green"></div>
                             <li>
-                                <p class="green">Er zijn 5 nieuwe reacties</p>
+                                <p class="green">You have 5 new messages</p>
                             </li>
                             <li>
-                                <a href="index.php#">
-                                    <span class="photo"><img alt="avatar" src="dashgum/Theme/assets/img/ui-zac.jpg"></span>
+                                <a href="../index.php#">
+                                    <span class="photo"><img alt="avatar" src="../dashgum/Theme/assets/img/ui-zac.jpg"></span>
                                     <span class="subject">
                                     <span class="from">Zac Snider</span>
                                     <span class="time">Just now</span>
@@ -79,8 +75,8 @@
                                 </a>
                             </li>
                             <li>
-                                <a href="index.php#">
-                                    <span class="photo"><img alt="avatar" src="dashgum/Theme/assets/img/ui-divya.jpg"></span>
+                                <a href="../index.php#">
+                                    <span class="photo"><img alt="avatar" src="../dashgum/Theme/assets/img/ui-divya.jpg"></span>
                                     <span class="subject">
                                     <span class="from">Divya Manian</span>
                                     <span class="time">40 mins.</span>
@@ -91,8 +87,8 @@
                                 </a>
                             </li>
                             <li>
-                                <a href="index.php#">
-                                    <span class="photo"><img alt="avatar" src="dashgum/Theme/assets/img/ui-danro.jpg"></span>
+                                <a href="../index.php#">
+                                    <span class="photo"><img alt="avatar" src="../dashgum/Theme/assets/img/ui-danro.jpg"></span>
                                     <span class="subject">
                                     <span class="from">Dan Rogers</span>
                                     <span class="time">2 hrs.</span>
@@ -103,8 +99,8 @@
                                 </a>
                             </li>
                             <li>
-                                <a href="index.php#">
-                                    <span class="photo"><img alt="avatar" src="dashgum/Theme/assets/img/ui-sherman.jpg"></span>
+                                <a href="../index.php#">
+                                    <span class="photo"><img alt="avatar" src="../dashgum/Theme/assets/img/ui-sherman.jpg"></span>
                                     <span class="subject">
                                     <span class="from">Dj Sherman</span>
                                     <span class="time">4 hrs.</span>
@@ -115,7 +111,7 @@
                                 </a>
                             </li>
                             <li>
-                                <a href="index.php#">See all messages</a>
+                                <a href="../index.php#">See all messages</a>
                             </li>
                         </ul>
                     </li>
@@ -125,12 +121,12 @@
             </div>
             <div class="top-menu">
             	<ul class="nav pull-right top-menu">
-                    <li><a class="logout" href="login.php">Logout</a></li>
+                    <li><a class="logout" href="../login.php">Logout</a></li>
             	</ul>
             </div>
         </header>
       <!--header end-->
-
+      
       <!-- **********************************************************************************************************************************************************
       MAIN SIDEBAR MENU
 *********************************************************************************************************************************************************** -->
@@ -139,30 +135,32 @@
           <div id="sidebar"  class="nav-collapse ">
               <!-- sidebar menu start-->
               <ul class="sidebar-menu" id="nav-accordion">
-              	  <h5 class="centered">Company Name</h5>
+              
+              	  <p class="centered"><a href="profiel.php"><img src="../dashgum/Theme/assets/img/ui-sam.jpg" class="img-circle" width="60"></a></p>
+              	  <h5 class="centered">Bedrijfs naam</h5>
 
                   <li class="mt">
-                      <a class="active" href="index.php">
+                      <a href="../index.php">
                           <i class="fa fa-dashboard"></i>
                           <span>Dashboard</span>
                       </a>
                   </li>
 
                   <li class="sub-menu">
-                      <a href="php/profiel.php">
+                      <a class="active" href="profiel.php" >
                           <i class="fa fa-desktop"></i>
                           <span>Profiel</span>
                       </a>
                   </li>
 
                   <li class="sub-menu">
-                      <a href="php/vacatures.php" >
+                      <a href="vacatures.php" >
                           <i class="fa fa-cogs"></i>
                           <span>Vacatures</span>
                       </a>
                   </li>
                   <li class="sub-menu">
-                      <a href="php/berichten.php" >
+                      <a href="berichten.php" >
                           <i class="fa fa-book"></i>
                           <span>Berichten</span>
                       </a>
@@ -172,68 +170,28 @@
           </div>
       </aside>
       <!--sidebar end-->
-
+      
       <!-- **********************************************************************************************************************************************************
       MAIN CONTENT
 *********************************************************************************************************************************************************** -->
       <!--main content start-->
       <section id="main-content">
-          <section class="wrapper">
-
-              <div class="row">
-                  <div class="col-lg-9 main-chart">
-
-                  	<div class="row mtbox">
-                  		<div class="col-md-2 col-sm-2 col-md-offset-1 box0">
-                  			<div class="box1">
-					  			<span class="li_mail"></span>
-					  			<h3>5</h3>
-                  			</div>
-					  			<p>5 new messages!</p>
-                  		</div>
-
-                  	</div><!-- /row mt -->
-
-
-                      <div class="row mt">
-                      <!-- SERVER STATUS PANELS -->
-                      	<div class="col-md-4 col-sm-4 mb">
-                      		<div class="white-panel pn donut-chart">
-                      			<div class="white-header">
-						  			<h5>SERVER LOAD</h5>
-                      			</div>
-								<div class="row">
-									<div class="col-sm-6 col-xs-6 goleft">
-										<p><i class="fa fa-database"></i> 70%</p>
-									</div>
-	                      		</div>
-								<canvas id="serverstatus01" height="120" width="120"></canvas>
-								<script>
-									var doughnutData = [
-											{
-                                                value: 70,
-												color:"#68dff0"
-											},
-											{
-                                                value : 30,
-												color : "#fdfdfd"
-											}
-										];
-										var myDoughnut = new Chart(document.getElementById("serverstatus01").getContext("2d")).Doughnut(doughnutData);
-								</script>
-	                      	</div><! --/grey-panel -->
-                      	</div><!-- /col-md-4-->
-
-                    </div><!-- /row -->
-              </div><! --/row -->
-          </section>
-      </section>
+          <section class="wrapper site-min-height">
+          	<h3><i class="fa fa-angle-right"></i> Blank Page</h3>
+          	<div class="row mt">
+          		<div class="col-lg-12">
+          		<p>Place your content here.</p>
+          		</div>
+          	</div>
+			
+		</section><! --/wrapper -->
+      </section><!-- /MAIN CONTENT -->
 
       <!--main content end-->
       <!--footer start-->
       <footer class="site-footer">
           <div class="text-center">
-              <a href="index.php#" class="go-top">
+              <a href="../dashgum/Theme/blank.html#" class="go-top">
                   <i class="fa fa-angle-up"></i>
               </a>
           </div>
@@ -242,61 +200,28 @@
   </section>
 
     <!-- js placed at the end of the document so the pages load faster -->
-    <script src="dashgum/Theme/assets/js/jquery.js"></script>
-    <script src="dashgum/Theme/assets/js/jquery-1.8.3.min.js"></script>
-    <script src="dashgum/Theme/assets/js/bootstrap.min.js"></script>
-    <script class="include" type="text/javascript" src="dashgum/Theme/assets/js/jquery.dcjqaccordion.2.7.js"></script>
-    <script src="dashgum/Theme/assets/js/jquery.scrollTo.min.js"></script>
-    <script src="dashgum/Theme/assets/js/jquery.nicescroll.js" type="text/javascript"></script>
-    <script src="dashgum/Theme/assets/js/jquery.sparkline.js"></script>
+    <script src="../dashgum/Theme/assets/js/jquery.js"></script>
+    <script src="../dashgum/Theme/assets/js/bootstrap.min.js"></script>
+    <script src="../dashgum/Theme/assets/js/jquery-ui-1.9.2.custom.min.js"></script>
+    <script src="../dashgum/Theme/assets/js/jquery.ui.touch-punch.min.js"></script>
+    <script class="include" type="text/javascript" src="../dashgum/Theme/assets/js/jquery.dcjqaccordion.2.7.js"></script>
+    <script src="../dashgum/Theme/assets/js/jquery.scrollTo.min.js"></script>
+    <script src="../dashgum/Theme/assets/js/jquery.nicescroll.js" type="text/javascript"></script>
 
 
     <!--common script for all pages-->
-    <script src="dashgum/Theme/assets/js/common-scripts.js"></script>
-
-    <script type="text/javascript" src="dashgum/Theme/assets/js/gritter/js/jquery.gritter.js"></script>
-    <script type="text/javascript" src="dashgum/Theme/assets/js/gritter-conf.js"></script>
+    <script src="../dashgum/Theme/assets/js/common-scripts.js"></script>
 
     <!--script for this page-->
-    <script src="dashgum/Theme/assets/js/sparkline-chart.js"></script>
-	<script src="dashgum/Theme/assets/js/zabuto_calendar.js"></script>
+    
+  <script>
+//custom select box
 
+$(function(){
+    $('select.styled').customSelect();
+});
 
-	<script type="application/javascript">
-    $(document).ready(function () {
-        $("#date-popover").popover({html: true, trigger: "manual"});
-            $("#date-popover").hide();
-            $("#date-popover").click(function (e) {
-                $(this).hide();
-            });
-
-            $("#my-calendar").zabuto_calendar({
-                action: function () {
-            return myDateFunction(this.id, false);
-        },
-                action_nav: function () {
-            return myNavFunction(this.id);
-        },
-                ajax: {
-            url: "show_data.php?action=1",
-                    modal: true
-                },
-                legend: [
-                    {type: "text", label: "Special event", badge: "00"},
-                    {type: "block", label: "Regular event", }
-                ]
-            });
-        });
-
-
-        function myNavFunction(id) {
-            $("#date-popover").hide();
-            var nav = $("#" + id).data("navigation");
-            var to = $("#" + id).data("to");
-            console.log('nav ' + nav + ' to: ' + to.month + '/' + to.year);
-        }
-    </script>
-
+  </script>
 
   </body>
 </html>
